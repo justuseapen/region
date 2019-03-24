1ce7b1233f0e46cd17d6bd8e66dfed937a1740a1
Giancarlo Bautista <gbautis@gmail.com>
2019-03-23 20:43:07 -0400

    Add createdAt field to Doc GraphQL type

--- lib/region_web/schema/types.ex
+++ lib/region_web/schema/types.ex
@@ -2,10 +2,13 @@ defmodule RegionWeb.Schema.Types do
   use Absinthe.Schema.Notation
   alias Region.Repo
 
+  import_types Absinthe.Type.Custom
+
   object :doc do
     field :id, :id
     field :title, :string
     field :body, :string
+    field :inserted_at, :naive_datetime, name: "created_at"
   end
 
   object :location_doc do
